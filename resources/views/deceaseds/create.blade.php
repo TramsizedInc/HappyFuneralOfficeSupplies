4ef96dab70c6d7d51ea3b17e6be5f71f8468ada2
milan <milan.maroevich@gmail.com>
2024-03-12 18:38:50 +0100

    picsa

--- resources/views/deceaseds/create.blade.php
+++ resources/views/deceaseds/create.blade.php
@@ -52,77 +52,91 @@
     <div class="p-8 mt-6 lg:mt-0 leading-normal rounded shadow bg-white">
     <form>
 
-<div class="md:flex mb-6">
-  <div class="md:w-1/4 me-2">
-    <input class="form-input block w-full focus:bg-white" id="my-textfield" type="text" placeholder="Megrendelő" required>
+<div class="md:flex mb-2">
+  <div class="md:w-1/5 me-2">
+    <input class="form-input block w-full focus:bg-white" type="text" placeholder="Neve" required>
     <p class="py-2 text-sm text-gray-600"></p>
   </div>
-  <div class="md:w-1/4 me-2">
-    <input class="form-input block w-full focus:bg-white" id="my-textfield" type="text" value="" placeholder="Megrendelő születési neve">
+  <div class="md:w-1/5 me-2">
+    <input class="form-input block w-full focus:bg-white" type="text" value="" placeholder="Születési neve">
     <p class="py-2 text-sm text-gray-600"></p>
   </div>
-  <div class="md:w-1/4 me-2">
-    <input class="form-input block w-full focus:bg-white" id="my-textfield" type="text" value="" placeholder="Megrendelő Anyja neve">
+  <div class="md:w-1/5 me-2">
+    <input class="form-input block w-full focus:bg-white" type="text" value="" placeholder="Anyja neve">
     <p class="py-2 text-sm text-gray-600"></p>
   </div>
-</div>
-<div class="md:flex mb-6">
-  <div class="md:w-1/3">
-    <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4" for="my-textfield">
-      Lakcím
-    </label>
+  <div class="md:w-1/5 me-2">
+    <input class="form-input block w-full focus:bg-white" type="text" value="" placeholder="Születési helye">
+    <p class="py-2 text-sm text-gray-600"></p>
   </div>
-  <div class="md:w-2/3">
-    <input class="form-input block w-full focus:bg-white" id="my-textfield" type="text" value="">
+  <div class="md:w-1/5 me-2">
+    <input datetimepicker  name="yearly_check_date" class="form-input block w-full focus:bg-white date-flatpickr" type="text" placeholder="Születési ideje" />
     <p class="py-2 text-sm text-gray-600"></p>
   </div>
 </div>
-<div class="md:flex mb-6">
-  <div class="md:w-1/3">
-    <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4" for="my-textfield">
-      Születési helye
-    </label>
+<div class="md:flex mb-2">
+  <label class="block text-gray-600 font-bold md:text-left md:mb-0 pr-4" for="my-textfield">
+    Lakcíme
+  </label>
+</div>
+<div class="md:flex mb-2">
+  <div class="md:w-2/8 me-2">
+    <input class="form-input block w-full focus:bg-white" type="text" value="" placeholder="Ország">
+    <p class="py-2 text-sm text-gray-600"></p>
   </div>
-  <div class="md:w-2/3">
-    <input class="form-input block w-full focus:bg-white" id="my-textfield" type="text" value="">
+  <div class="md:w-2/8 me-2">
+    <input class="form-input block w-full focus:bg-white"  type="text" value="" placeholder="Irányítószám">
+    <p class="py-2 text-sm text-gray-600"></p>
+  </div>
+  <div class="md:w-1/8 me-2">
+    <input class="form-input block w-full focus:bg-white" type="text" value="" placeholder="Város">
+    <p class="py-2 text-sm text-gray-600"></p>
+  </div>
+  <div class="md:w-2/8 me-2">
+    <input class="form-input block w-full focus:bg-white"  type="text" value="" placeholder="Utca">
+    <p class="py-2 text-sm text-gray-600"></p>
+  </div>
+  <div class="md:w-1/8 me-2">
+    <input class="form-input block w-full focus:bg-white"  type="text" value="" placeholder="Házszám">
     <p class="py-2 text-sm text-gray-600"></p>
   </div>
 </div>
-<div class="md:flex mb-6">
-  <div class="md:w-1/3">
-    <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4" for="my-textfield">
-      Születési ideje
-    </label>
-  </div>
-  <div class="md:w-2/3">
-     <input datetimepicker id="yearly_check_date" name="yearly_check_date" class="form-input block w-full focus:bg-white date-flatpickr" type="text" placeholder="Válassz időpontot" />
-     <p class="py-2 text-sm text-gray-600"></p>
-  </div>
+<div class="md:flex mb-2">
+  <label class="block text-gray-600 font-bold md:text-left md:mb-0 pr-4" for="my-textfield">
+    Elérhetőségei
+  </label>
 </div>
-<div class="md:flex mb-6">
-    <div class="md:w-1/3">
-        <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4" for="my-textarea">
-            Telefonszám
-        </label>
+<div class="md:flex mb-2">
+  <div class="relative md:w-1/2 me-2">
+    <div class="absolute inset-y-0 start-0 top-0 flex items-center ps-3.5 pointer-events-none">
+        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
+            <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
+        </svg>
     </div>
-    <div class="relative md:w-2/3">
-        <div class="absolute inset-y-0 start-0 top-0 flex items-center ps-3.5 pointer-events-none">
-            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 19 18">
-                <path d="M18 13.446a3.02 3.02 0 0 0-.946-1.985l-1.4-1.4a3.054 3.054 0 0 0-4.218 0l-.7.7a.983.983 0 0 1-1.39 0l-2.1-2.1a.983.983 0 0 1 0-1.389l.7-.7a2.98 2.98 0 0 0 0-4.217l-1.4-1.4a2.824 2.824 0 0 0-4.218 0c-3.619 3.619-3 8.229 1.752 12.979C6.785 16.639 9.45 18 11.912 18a7.175 7.175 0 0 0 5.139-2.325A2.9 2.9 0 0 0 18 13.446Z"/>
-            </svg>
-        </div>
-        <input type="text" id="phone-input" aria-describedby="helper-text-explanation" class="ps-10 p-2.5  form-input block w-full focus:bg-white" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="123-456-7890" required />
-    </div>
+    <input type="text" id="phone-input" aria-describedby="helper-text-explanation" class="ps-10  form-input block w-full focus:bg-white" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="Telenfonszám" required />
+  </div>
+
+  <div class="md:w-1/2">
+    <input type="email" id="email" class="form-input block w-full focus:bg-white" placeholder="Email" required />   
+  </div>  
 </div>
 
-<div class="md:flex mb-6">
-  <div class="md:w-1/3">
-    <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4" for="my-textarea">
-      Email
-    </label>
+<div class="md:flex mb-2">
+  <div class="md:w-1/4 me-2">
+    <input class="form-input block w-full focus:bg-white" type="text" placeholder="Neve" required>
+    <p class="py-2 text-sm text-gray-600"></p>
   </div>
-  <div class="md:w-2/3">
-  <input type="email" id="email" class="form-input block w-full focus:bg-white" placeholder="név@temetkezes.hu" required />   
+  <div class="md:w-1/4 me-2">
+    <input class="form-input block w-full focus:bg-white" type="text" value="" placeholder="Születési neve">
+    <p class="py-2 text-sm text-gray-600"></p>
+  </div>
+  <div class="md:w-1/4 me-2">
+    <input class="form-input block w-full focus:bg-white" type="text" value="" placeholder="Anyja neve">
+    <p class="py-2 text-sm text-gray-600"></p>
+  </div>
+  <div class="md:w-1/4 me-2">
+    <input class="form-input block w-full focus:bg-white" type="text" value="" placeholder="Születési helye">
+    <p class="py-2 text-sm text-gray-600"></p>
   </div>
 </div>
 
@@ -205,17 +219,7 @@
      <p class="py-2 text-sm text-gray-600"></p>
   </div>
 </div>
-<div class="md:flex mb-6">
-  <div class="md:w-1/3">
-    <label class="block text-gray-600 font-bold md:text-left mb-3 md:mb-0 pr-4" for="my-textfield">
-    Megrendelő születési helye
-    </label>
-  </div>
-  <div class="md:w-2/3">
-    <input class="form-input block w-full focus:bg-white" id="my-textfield" type="text" value="">
-    <p class="py-2 text-sm text-gray-600"></p>
-  </div>
-</div>
+
 <div class="md:flex md:items-center">
   <div class="md:w-1/3"></div>
   <div class="md:w-2/3">
