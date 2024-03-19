7e7ceef2382dc63e12fab88949af298b94dfdc0c
milan <milan.maroevich@gmail.com>
2024-03-19 14:44:53 +0100

    Error page

--- resources/views/order_data/create.blade.php
+++ resources/views/order_data/create.blade.php
@@ -100,7 +100,7 @@
                             </div>
                             <div class="text-gray-600 md:col-span-5">
                                 <div class="relative mb-6">
-                                    <label for="labels-range-input" >Doner <span id="donerchange"></span></label>
+                                    <label for="labels-range-input" >Tonner <span id="donerchange"></span></label>
                                     <input id="labels-range-input" onchange="donerchange(this)" oninput="donerchange(this)" type="range" value="0" min="0" max="100" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
                                     <span class="text-sm text-gray-500 dark:text-gray-400 absolute start-0 -bottom-6">0%</span>
                                     <span class="text-sm text-gray-500 dark:text-gray-400 absolute start-1/2 -translate-x-1/2 rtl:translate-x-1/2 -bottom-6">50%</span>
