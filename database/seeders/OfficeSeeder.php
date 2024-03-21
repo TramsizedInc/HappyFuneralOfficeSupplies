d76e2c4f87bb1dad4fef9a1de4d18c5b41e26538
milan <milan.maroevich@gmail.com>
2024-03-21 12:39:59 +0100

    create form

--- database/seeders/OfficeSeeder.php
+++ database/seeders/OfficeSeeder.php
@@ -13,22 +13,6 @@ class OfficeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
-        Office::create([
-            "office_name"=> "dummyOffice",
-            "zip_code" => "2336",
-            "city" => "Sunavarsány",
-            "street" => "Bokréta u.",
-            "house_number" => "10",
-            "number_of_workers" => "2"
-        ]);
-        Office::create([
-            "office_name"=> "dummyOffice",
-            "zip_code" => "1076",
-            "city" => "Pinaváros",
-            "street" => "Verseny u.",
-            "house_number" => "6",
-            "number_of_workers" => "2"
-        ]);
+
     }
 }
